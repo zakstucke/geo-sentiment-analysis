ec33a33ad2aa0f17458a6ed9c3eea5a049f743c3
sl19692 <sl19692@bristol.ac.uk>
2022-04-05 14:59:13 +0100

    Added hypothesis testing to correlation visualisation

--- casesSentiment.py
+++ casesSentiment.py
@@ -5,6 +5,7 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 import pycountry
+from scipy.stats import pearsonr
 
 pd.options.mode.chained_assignment = None  # default='warn' this is needed for false positives warnings on reassigning pd dataframe
 
@@ -15,7 +16,7 @@ WHOdf = pd.read_csv(WHOfile, usecols=["iso_code", "date", "new_cases"])
 #TWEET DATA FROM PREPROCESSED DF
 with open("./processed_data/final_df.json", "r") as file:
     COVIDdf = pd.read_json(json.load(file))
-    
+
 #print(COVIDdf.head(20))
 #lookup iso-3 country code
 def normaliseCC(row):
@@ -43,7 +44,7 @@ def groupDFByMonth(iso_col_name, code, df):
 def plot(WHOdf, COVIDdf, iso_code):
     whoiso = groupDFByMonth("iso_code", iso_code, WHOdf)
     covidiso = groupDFByMonth("iso_code", iso_code, COVIDdf)
-    
+
     title = iso_code + " Avg Daily New Cases By Month over Covid with Average Sentiment of Tweets Regarding COVID"
     whoiso.join(covidiso)
     df = pd.merge(whoiso, covidiso, left_index=True, right_index=True)
@@ -60,17 +61,24 @@ def plot(WHOdf, COVIDdf, iso_code):
     ax2.plot(df.date, df.compound, color='blue')
     ax2.set_ylabel("Average Sentiment", color='blue')
 
-       
+    #Generate test statistic
+    p, corr, result = hypothesisTest(df.new_cases, df.compound, 0.05)
+    plt.text(min(df.date),max(df.compound),"Correlation: " + str(corr), fontsize=13)
     plt.show()
-    
+
+def hypothesisTest(x, y, alpha):
+    likelyDependent = False
+    corr, p = pearsonr(x, y)
+    print('Correlation=%.3f, p=%.3f' % (corr, p))
+    if p < alpha:
+	       likelyDependent = True
+    return p, corr, likelyDependent
+
 
 COVIDdf = cleanTweetDF(COVIDdf)
 
 #ENTER ANY COUNTRY CODE (SOME HAVE MORE TWEETS THAN OTHERS SO WILL SHOW BETTER RESULTS)
 plot(WHOdf, COVIDdf, "RUS")
 plot(WHOdf, COVIDdf, "USA")
-plot(WHOdf, COVIDdf, "DEU") 
-plot(WHOdf, COVIDdf, "CAN") 
-
-
-
+plot(WHOdf, COVIDdf, "DEU")
+plot(WHOdf, COVIDdf, "CAN")
