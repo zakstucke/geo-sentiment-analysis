325c71e2593e01a09c1cc09c83f00ccf99b305d4
Edward Jinks <edwardjinks12@gmail.com>
2022-05-01 16:29:40 +0100

    reformat state df

--- hypothesis.py
+++ hypothesis.py
@@ -8,18 +8,16 @@ import numpy as np
 
 pd.options.mode.chained_assignment = None  # default='warn' this is needed for false positives warnings on reassigning pd dataframe
 
-WHOfile = "data/WHO_covid_data_by_COUNTRY.csv"
-WHOdf = pd.read_csv(WHOfile, usecols=["iso_code", "date", "new_cases", "new_vaccinations", "new_tests"])
+# WHOfile = "data/WHO_covid_data_by_COUNTRY.csv"
+# WHOdf = pd.read_csv(WHOfile, usecols=["iso_code", "date", "new_cases", "new_vaccinations", "new_tests"])
 
-#COVIDdf = pd.read_csv('cleanedForCaseSentiment.csv') #just needs to read as saved processed to the file to speed up testing
+# #COVIDdf = pd.read_csv('cleanedForCaseSentiment.csv') #just needs to read as saved processed to the file to speed up testing
 
-COVIDdf = pd.read_csv('processed_data/final_csv.txt')
-COVIDdf.rename(columns={'created_at': 'date'}, inplace=True)
+# COVIDdf = pd.read_csv('processed_data/final_csv.txt')
+# COVIDdf.rename(columns={'created_at': 'date'}, inplace=True)
 
 
 dictionary = {'AK': (0.41907715530782635, -0.24537150333353125, False), 'AL': (0.4315520381744366, -0.21963934838989863, False), 'AR': (0.13407237538852396, 0.4051755925832934, False), 'AZ': (0.3212129960180458, 0.2749990046380907, False), 'CA': (0.6855092544793175, 0.11411679678375226, False), 'CO': (0.6833996163461826, 0.11492064277013786, False), 'CT': (0.5433341170582301, -0.17770218018133316, False), 'DE': (0.4792840875630574, -0.2062568089495694, False), 'FL': (0.18831065942801714, 0.35937872123108605, False), 'GA': (0.8003174166209478, 0.07142076135664802, False), 'HI': (0.7562957447954272, -0.08757518704265445, False), 'IA': (0.47182924861309433, 0.20132588032947757, False), 'ID': (0.9732421246806225, 0.009483204741854878, False), 'IL': (0.9709297051593461, 0.010303051070367455, False), 'IN': (0.500298389329485, 0.1888375218959172, False), 'KS': (0.397512765260647, -0.2358065847768691, False), 'KY': (0.2544276875974118, 0.31397971329007707, False), 'LA': (0.11679369277977635, 0.42237181070645125, False), 'MA': (0.2960879530011792, 0.2890436517992506, False), 'MD': (0.644346181122446, 0.12995936142950362, False), 'ME': (0.09386789628421847, 0.6783966311196863, False), 'MI': (0.12132588901961158, 0.4177010819208654, False), 'MN': (0.1484085287943251, 0.39201978748331956, False), 'MO': (0.3564551173729067, -0.2563206322097375, False), 'MS': (0.7743182578062676, -0.08093359291951864, False), 'MT': (0.9079078424730914, 0.0340855240442699, False), 'NC': (0.26404247322043256, 0.30801829457120455, False), 'ND': (0.5908149383172604, -0.1574745952847917, False), 'NE': (0.01075410820374891, 0.6363684522839479, True), 'NH': (0.1927194923276158, 0.6161643510994979, False), 'NJ': (0.09656103245821505, -0.44491019176575564, False), 'NM': (0.5441437400688679, -0.17023101878185642, False), 'NV': (0.014757297453465035, 0.614654270738406, True), 'NY': (0.8874601461610407, -0.03999548859326904, False), 'OH': (0.9783820328642071, -0.0076611414900575325, False), 'OK': (0.8724074615467825, -0.045383251867331505, False), 'OR': (0.8904055505250477, -0.03894279745074948, False), 'PA': (0.7077821261394852, 0.10567868849719592, False), 'RI': (0.2496628670426442, 0.316985065020106, False), 'SC': (0.6548694581595924, 0.12587622427167605, False), 'SD': (0.5941287087067796, 0.14979983565978972, False), 'TN': (0.7458634602083715, -0.09143910296752672, False), 'TX': (0.21811986844477982, 0.33783836907188647, False), 'UT': (0.9341766107063042, -0.023347805488491746, False), 'VA': (0.3134699331092026, 0.4991383319331629, False), 'VT': (0.6179624692372041, 0.5647276037761855, False), 'WA': (0.5144893553777465, 0.1827371902114813, False), 'WI': (0.39372265439925486, -0.23765092342379612, False), 'WV': (1.0, -1.0, False), 'WY': (0.7134379828466156, 0.14308452894003304, False)}
-
-
 df = pd.DataFrame(dictionary)
 df.to_csv('stateCorrelation.csv')
 
@@ -128,10 +126,15 @@ def vaxTweetFilter(df):
     return df
 
 
-def stateVaccineTable(df):
+def stateBarChart(df):
+    df = df.transpose()
+    print(df)
+    df = df.rename(columns={0: "p", 1: "corr", 2: "likelyDependent"})
+    print(df)
     return df
 
-
+stateDF = pd.read_csv('stateCorrelation.csv')
+stateBarChart(stateDF)
 
 #vaxTweetFilter(COVIDdf)
 
